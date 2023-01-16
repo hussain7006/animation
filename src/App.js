805052c8a45cf3c4bd64681aa7edf70989bd33e3
MUHAMMAD HUSSAIN <M.HUSSAIN7006@GMAIL.COM>
2023-01-16 21:56:00 +0500

    zoom image and canvas pages added

--- src/App.js
+++ src/App.js
@@ -4,13 +4,17 @@ import './App.css';
 import Border2 from './components/Border2/Border2'
 import GlowingRing from './components/GlowingRing/GlowingRing';
 import WaterDropCard from './components/WaterDropCard/WaterDropCard';
+import Canvas from './components/Canvas/Canvas';
+import ZoomImage from './components/ZoomImage/ZoomImage';
 
 function App() {
   return (
-    <div className="App">
+    <div className="">
       {/* <Border2 /> */}
       {/* <GlowingRing /> */}
-      <WaterDropCard />
+      {/* <WaterDropCard /> */}
+      {/* <Canvas /> */}
+      {/* <ZoomImage /> */}
     </div>
   );
 }
